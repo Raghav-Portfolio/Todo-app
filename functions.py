819ea0fa34edc6d3c01db4687e3645bcf2e0a468
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-01 11:04:51 +0530

    Implemented the Add Button. Edit, Remove and Exit to come soon.

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-def get_todos(filepath):
+FILEPATH='todos.txt'
+
+def get_todos(filepath=FILEPATH):
     with open(filepath,'r') as file:
         todos_local=file.readlines()
     return todos_local
 
-def write_todos(filename,todos_write):
+def write_todos(todos_write,filename=FILEPATH):
         with open(filename,'w') as file:
             file.writelines(todos_write)
--- gui.py
+++ gui.py
@@ -1,9 +1,28 @@
 import FreeSimpleGUI as fsg
+import functions
+
 
 label=fsg.Text('Add To-Do App')
-input=fsg.InputText(tooltip='Enter To-Do Item')
+input=fsg.InputText(tooltip='Enter To-Do Item', key='to-do')
 button=fsg.Button('Add')
  
-window=fsg.Window('My To-Do App', layout=[[label],[input, button]])
-window.read()
-window.close()
+window=fsg.Window('My To-Do App', 
+                  layout=[[label],[input, button]], 
+                  font=('Helvetica', 16)
+                  )
+while True:
+    event, values= window.read() 
+    #event get the value of the button's name, i.e. 'Add' in this case.
+    #Values get the value of the "key" argument of the InputText method, i.e. 'to-do' in this case.
+    
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values['to-do'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case fsg.WIN_CLOSED:    
+            break
+window.close()  
